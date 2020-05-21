170d269f1f6a32b50c93ca27ef724a34d887c792
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 13:37:34 +0530

    store is assigned rootreducer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { reducer as formReducer } from "redux-form";
+import { createStore, combineReducers } from "redux";
 //reducer imported from "redux-form" not from "redux"
 //we have changed name here b'coz our project code have multiple reducer
 //so to avoid name conflict
+//form is key for "formReducer"
+//react-redux & redux imported
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
     <App />
