c221cc8da208460def27e051d5e849803d65eab7
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 13:44:48 +0530

    field and redux form imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Field, reduxForm } from "redux-form";
 
 import "./App.css";
-//normally when we make a form in redux
-//we make action /reducer
-//this incresase line of codes
-//Redux form automates the this process
-//&reduces no of lines of code
-//now we don't need "reducer" from "redux" package
-//now "reducer" will be used from "redux-form package"
+
 function App() {
   return (
     <div className="App">
