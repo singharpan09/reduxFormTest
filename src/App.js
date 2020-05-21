ffa945fc96276f4bdb7f05a3cb37c58d19aa14cd
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 13:26:59 +0530

    need of redux form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 import "./App.css";
-
+//normally when we make a form in redux
+//we make action /reducer
+//this incresase line of codes
+//Redux form automates the this process
+//&reduces no of lines of code
 function App() {
   return (
     <div className="App">
