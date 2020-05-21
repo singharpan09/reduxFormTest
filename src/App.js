d122be530304ad01c0652b12bd022985476c1af3
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 13:53:57 +0530

    first field initlized

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import { Field, reduxForm } from "redux-form";
 
 import "./App.css";
-
+//Field is used to create input fields
+//there is some necessary "props" to be provided to it
 function App() {
   return (
     <div className="App">
       <h3>This is redux form</h3>
+      <label>First name</label>
+      <Field type="text" name="firstname" component="input" />
     </div>
   );
 }
@@ -14,5 +17,8 @@ function App() {
 export default reduxForm({
   form: "myform",
 })(App);
+//component="input" --tells that it is input tag
+//type="text"---tells it is of type text
+//name field is also necessary
 //now the component will be wrapped with redux form  like ...connect
 //we have to pass a object to redux form ...with key....& a unique name
