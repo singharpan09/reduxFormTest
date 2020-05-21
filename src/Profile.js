19abef47108b8e4402b4a8ef3f41ad8d8655cd90
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 14:30:30 +0530

    profile to use the form data

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+//now we will use all those from values here
+//now will use connect here
+import React from "react";
+
+import "./App.css";
+
+function Profile(props) {
+  return (
+    <div className="App">
+      <h3>This is redux form</h3>
+    </div>
+  );
+}
+
+export default Profile;
