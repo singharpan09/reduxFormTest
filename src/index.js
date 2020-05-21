1463e27c0c9c18edcfd06d1cb6c289a1cc736652
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 13:40:04 +0530

    provider used

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ import "./index.css";
 import App from "./App";
 import { reducer as formReducer } from "redux-form";
 import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 //reducer imported from "redux-form" not from "redux"
 //we have changed name here b'coz our project code have multiple reducer
 //so to avoid name conflict
-//form is key for "formReducer"
+//form is key for "formReducer" ..it should be same
 //react-redux & redux imported
 const rootReducer = combineReducers({
+  //there can be multiple reducers here
   form: formReducer,
 });
 
 const store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
